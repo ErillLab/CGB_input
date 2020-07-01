290caf42d637c9c4481b6692f8729c38f6493a28
shane806 <47383634+shane806@users.noreply.github.com>
2020-07-01 07:07:10 -0400

    Add files via upload

--- cgb_input_run.py
+++ cgb_input_run.py
@@ -1,8 +1,5 @@
 from cgb_input import create_file
 
-input_file = 'pseudomonadales_test.json'
+input_file = 'test_input.json'
 
 input_file = create_file(input_file)
-
-with open('orthologs.json') as f:
-    orth = json.load(f)
