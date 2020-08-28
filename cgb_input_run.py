b82f6dbc5197090dcac9f494b2959fdba656d804
shane806 <47383634+shane806@users.noreply.github.com>
2020-08-28 13:41:37 -0400

    Update cgb_input_run.py
    just changed the position of INPUT_FILE variable. There is also code now that will print to the terminal how long the program took to run.

--- cgb_input_run.py
+++ cgb_input_run.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
+INPUT_FILE = 'test_input.json'
+
 import time
 
 from cgb_input import create_file
 
 seconds_per_minute = 60
 
-input_file = 'pseudomon_family.json'
-
 start_time = time.time()
 
-cgb_input_file = create_file(input_file)
+cgb_input_file = create_file(INPUT_FILE)
 
 end_time = time.time()
 
